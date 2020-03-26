1e206450404f5044d58e27dd7bc128d6fece5966
Astrid van Aggelen <aevanaggelen@gmail.com>
2020-03-26 13:37:14 +0100

    minimal example for scale-up test

---
+++ vars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DIR_DATA_MAC="/Users/Astrid/minimal_example2"
+DIR_DATA_CWI="/export/scratch1/aggelen/Stereotypes/data/minimal_example2"
+NR_IN="/export/scratch2/home/aggelen/input_newsreader" #on alice
+NR_OUT="/export/scratch2/home/aggelen/output_newsreader" #on alice
+MP_IN="/export/scratch2/home/aggelen/micro-portraits/input" #on alice
+MP_OUT="/export/scratch2/home/aggelen/micro-portraits/output" #on alice
+CRR_IN=$NR_OUT
+CRR_OUT="/export/scratch2/home/aggelen/output_newsreader_coreferenced/minimal_example"
+FILE="a0000000"
+VARSFILE="vars.sh" #exists in
+CODE_CWI="/ufs/aggelen/Stereotypes/scripts"
+CODE_MAC="/Users/Astrid/Stereotypes/scripts"
